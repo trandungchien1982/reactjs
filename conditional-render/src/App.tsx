9c1ffe63bb0f319e852db853ee7dc78d87f32783
tdc <tdchien@yahoo.com>
2026-02-19 13:55:10 +0700

    Start 06.Conditional-Render

---
+++ conditional-render/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FC01 from './comps/FC01'
+import FC02 from './comps/FC02'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [currentPrice, setCurrentPrice] = useState(120);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <h1>Function Component + useProp()</h1>
+        <FC01 price={currentPrice} year="1000" background="red" updatePrice={(val: number) => {
+            console.log(" -- call updatePrice() in parent : " + val);
+            setCurrentPrice(val);
+        }} />
+        <hr />
+
+        <h1>Function Component + useProp()</h1>
+        <FC01 price="500" year="6000"/>
+        <hr />
+
+
+
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
