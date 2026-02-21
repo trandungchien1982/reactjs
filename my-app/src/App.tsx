49f3e88287238a8c0f0e3eb17e8313b02bc7b4aa
Tran Dung Chien <trandungchien1982@gmail.com>
2026-02-21 14:28:42 +0700

    Start 09.Composition-CustomHooks

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -10,17 +10,6 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-      // Giả lập việc tải dữ liệu
-      setTimeout(() => {
-        setData("Đây là dữ liệu từ API - " + new Date());
-        setLoading(false);
-      }, 2000);
-    }, []);
-
-  // Áp dụng HOC để tạo component mới có tính năng loading
-  const MyDataDisplayWithLoading = withLoading(MyDataDisplay);
-
   return (
     <>
       <div>
@@ -31,15 +20,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>08.Composition-HOCs</h1>
+      <h1>09.Composition-CustomHooks</h1>
       <div className="card">
-        <h1>React-Roadmap (08.Composition-HOCs) - layout Higher-Order Components (HOCs) </h1>
+        <h2>React-Roadmap (09.Composition - Custom Hooks) - layout </h2>
         <a target="_blank" href="https://tuyendung.evotek.vn/ket-hop-component-trong-react-tai-su-dung-that-de-dang-react-roadmap/">https://tuyendung.evotek.vn/ket-hop-component-trong-react-tai-su-dung-that-de-dang-react-roadmap/</a>
       </div>
       <div>
-        <h2>Ứng dụng sử dụng HOC</h2>
-        {/* Component này giờ đây nhận thêm prop 'isLoading' */}
-        <MyDataDisplayWithLoading isLoading={loading} data={data} />
+        ...
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
