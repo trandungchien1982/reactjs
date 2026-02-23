7a46d774461c5894a764d34c0d8a9e2c0db16288
Tran Dung Chien <trandungchien1982@gmail.com>
2026-02-23 08:33:22 +0700

    Start 21.useMemo

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,9 +2,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from './comps/Button'
-import Parent from './comps/Parent'
-import ParentWithCallback from './comps/ParentWithCallback'
+import Calculator from './comps/Calculator'
 
 function App() {
   return (
@@ -17,19 +15,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>20.useCallback</h1>
+      <h1>21.useMemo</h1>
       <div className="card">
-        <h2>React-Roadmap (20.useCallback) - Hooks</h2>
-        <a target="_blank" href="https://tuyendung.evotek.vn/khi-nao-va-vi-sao-nen-dung-usecallback-usememo-va-useref-react-roadmap/#useCallback_Ghi_nho_Memoizing_Ham">https://tuyendung.evotek.vn/khi-nao-va-vi-sao-nen-dung-usecallback-usememo-va-useref-react-roadmap/#useCallback_Ghi_nho_Memoizing_Ham</a>
+        <h2>React-Roadmap (21.useMemo) - Hooks</h2>
+        <a target="_blank" href="https://tuyendung.evotek.vn/khi-nao-va-vi-sao-nen-dung-usecallback-usememo-va-useref-react-roadmap/#useMemo_Ghi_nho_Memoizing_Gia_tri">https://tuyendung.evotek.vn/khi-nao-va-vi-sao-nen-dung-usecallback-usememo-va-useref-react-roadmap/#useMemo_Ghi_nho_Memoizing_Gia_tri</a>
       </div>
       <div>
           <h2>Content</h2>
-          <h3>Parent - normal - Make Parent re-render and then all buttons re-render: [Click Me], [Another Button]</h3>
-          <Parent />
-
-          <hr/>
-          <h3>Parent with useCallback - Parent re-render and then only inner buttons re-render: [Another Button]</h3>
-          <ParentWithCallback />
+          <h3>Calculator with useMemo</h3>
+          <Calculator />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ my-app/src/comps/Calculator.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Calculator() {
+  return (
+    <div>
+      Calculator Cnt
+    </div>
+  );
+}
+
+export default Calculator;
